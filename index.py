8d13410188ad4a91aec0625a7f753676998431f8
Victor Luiz <139981746+vluizlr@users.noreply.github.com>
2024-12-05 08:45:14 -0300

    Update and rename index.html to index.py

---
+++ index.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    @abstractmethod
+    def emitir_som(self):
+        pass
+
+class Cachorro(Animal):
+    def emitir_som(self):
+        print("Au au!")
+
+class Gato(Animal):
+    def emitir_som(self):
+        print("Miau!")
+
